6e0aaf403d904ea3e517ece57255e34e4f999300
Abhishek Sharma <sharma312006@gmail.com>
2023-11-21 06:36:49 +0530

    route and controllers

--- src/app.js
+++ src/app.js
@@ -25,4 +25,10 @@ app.use(express.static("public"));
 
 //cookie se data
 app.use(cookieParser());
+
+//router input
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
 export { app };
