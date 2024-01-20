d49bbf5890557b747d53522d5085aa787b4115ea
Abhishek Sharma <sharma312006@gmail.com>
2024-01-21 03:14:48 +0530

    getChhanrlStats

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -8,12 +8,17 @@ import {
   getUserPlaylists,
   removeVideoFromPlaylist,
   getPlayListById,
+  deletePlaylist,
 } from "../controllers/playlist.controller.js";
 
 router.use(verifyJWT);
 
 router.route("/").post(createPlaylist);
-router.route("/:playlistId").patch(updatePlaylist).get(getPlayListById);
+router
+  .route("/:playlistId")
+  .patch(updatePlaylist)
+  .get(getPlayListById)
+  .delete(deletePlaylist);
 router.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist);
 router.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
 router.route("/user/:userId").get(getUserPlaylists);
